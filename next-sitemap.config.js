bfff7c2d19168b42936587e818850ddeb9faec16
Matryoshka <17673942507@163.com>
2025-09-01 15:41:56 +0800

    feat: sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,24 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://lixiaowei11111.github.io',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  outDir: 'out', // 因为您使用的是output: "export"，所以输出目录是 "out"
+  exclude: [
+    '/api/*',
+    '/server-sitemap.xml',
+    '/404',
+    '/500'
+  ],
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      'https://lixiaowei11111.github.io/server-sitemap.xml', // 如果您有动态生成的页面
+    ],
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/'
+      }
+    ]
+  }
+}
